e186a5d252a1e28f961e62be31b268dc5553cd29
= <=>
2025-05-18 14:45:51 +0200

    funcio esta_normal funcionant correctament @juanjogomezvillegas

--- codi.hs
+++ codi.hs
@@ -1,3 +1,4 @@
+import Distribution.PackageDescription (Condition(Var))
 -- Pràctica de Haskell
 -- Copyright (c) 2025 Juan José Gómez Villegas (u1987338@campus.udg.edu), Company (uCompany@campus.udg.edu)
 
@@ -46,11 +47,11 @@ freeAndboundVarsAux (Variable a) freeVars boundVars = if a `elem` boundVars then
 --subst :: LT -> Substitucio -> LT
 
 -- esta_normal, diu si LT ja està en forma normal
---esta_normal :: LT -> Bool
---esta_normal (Variable a) = True
---esta_normal t | t == (Variable _) = True
---              | t == (Aplicacio t1 t2) = (esta_normal t1) (&&) (esta_normal t2)
---              | t == (Abstraccio a t1) = (esta_normal t1)
+esta_normal :: LT -> Bool
+esta_normal (Variable a) = True
+esta_normal (Aplicacio (Abstraccio _ _) _) = False
+esta_normal (Abstraccio _ t1) = (esta_normal t1)
+esta_normal (Aplicacio t1 t2) = (&&) (esta_normal t1) (esta_normal t2)
 
 -- beta_redueix, rep un LT que sigui un redex, i el resol
 --beta_redueix :: LT -> LT
@@ -78,13 +79,52 @@ freeAndboundVarsAux (Variable a) freeVars boundVars = if a `elem` boundVars then
 
 -- de_deBruijn :: LTdB -> LT
 
+-- Alguns combinadors i definicions del meta-llenguatge
+iden :: LT
+iden = (Abstraccio "x" (Variable "X"))
 
-exemple :: LT
-exemple = Abstraccio "x" (Aplicacio (Variable "x") (Variable "y"))
+true :: LT
+true = (Abstraccio "x" (Abstraccio "y" (Variable "x")))
 
-exempleDos :: LT
-exempleDos = Abstraccio "y" (Aplicacio (Variable "y") (Aplicacio (Variable "z") (Variable "c")))
+false :: LT
+false = (Abstraccio "x" (Abstraccio "y" (Variable "y")))
+
+not :: LT
+not = (Abstraccio "t" (Aplicacio false true))
+
+cond :: LT -> LT -> LT -> LT
+cond e e1 e2 = (Aplicacio (Aplicacio e e1) e2)
+
+and :: LT
+and = (Abstraccio "x" (Abstraccio "y" (cond (Variable "x") (Variable "y") false)))
+
+tupla :: LT
+tupla = (Abstraccio "x" (Abstraccio "y" (Abstraccio "p" (Aplicacio (Aplicacio (Variable "p") (Variable "x")) (Variable "y")))))
+
+first :: LT
+first = (Abstraccio "x" (Aplicacio (Variable "x") true))
+
+second :: LT
+second = (Abstraccio "x" (Aplicacio (Variable "x") false))
+
+succ :: LT
+succ = (Abstraccio "n" (Abstraccio "f" (Abstraccio "x" (Aplicacio ((Aplicacio (Variable "n") (Variable "f"))) ((Aplicacio (Variable "f") (Variable "x")))))))
+
+suma :: LT
+suma = (Abstraccio "m" (Abstraccio "n" (Abstraccio "f" (Abstraccio "x" (Aplicacio ((Aplicacio (Variable "m") (Variable "f"))) ((Aplicacio (Aplicacio (Variable "n") (Variable "f")) (Variable "x"))))))))
+
+producte :: LT
+producte = (Abstraccio "m" (Abstraccio "n" (Abstraccio "f" (Abstraccio "x" ((Aplicacio (Aplicacio (Variable "m") (Aplicacio (Variable "n") (Variable "f"))) (Variable "x")))))))
+
+eszero :: LT
+eszero = (Abstraccio "n" (Aplicacio (Aplicacio (Variable "n") (Abstraccio "x" false)) true))
+
+y :: LT
+y = (Abstraccio "f" (Aplicacio (Abstraccio "x" (Aplicacio (Variable "f") (Aplicacio (Variable "x") (Variable "x")))) (Abstraccio "x" (Aplicacio (Variable "f") (Aplicacio (Variable "x") (Variable "x"))))))
+
+t :: LT
+t = (Aplicacio (Abstraccio "x" (Abstraccio "y" (Aplicacio (Variable "y") (Aplicacio (Aplicacio (Variable "x") (Variable "x")) (Variable "y"))))) (Abstraccio "x" (Abstraccio "y" (Aplicacio (Variable "y") (Aplicacio (Aplicacio (Variable "x") (Variable "x")) (Variable "y"))))))
 
 main :: IO ()
 main = do
-    putStrLn(show exemple)
+    putStrLn(show t)
